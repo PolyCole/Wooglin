e90c23f508057c89e24b0b16d85500ebf2ad1770
Cole Polyak <colepolyak@gmail.com>
2019-10-31 19:00:27 +0100

    Reworked generating training data

--- scripts/trainWit.py
+++ scripts/trainWit.py
@@ -1,30 +1,59 @@
-import sys, os
+import sys
 from wit import Wit
 import time
+import random
 
-witClient = Wit(input("Please enter API key"))
+witClient = Wit(input("Please enter API key:"))
+
+
+def initialize(filename):
+    to_return = {}
+
+    if filename == "ListFile":
+        input_file = open("C:\\Users\\Five\\PycharmProjects\\Wooglin\\scripts\\attendanceTracking\\ListFile.txt", "r")
+    elif filename == "witData":
+        input_file = open("C:\\Users\\Five\\PycharmProjects\\Wooglin\\scripts\\witData.txt", "r")
+    else:
+        print("ERROR, NOT VALID FILENAME")
+        return None
+
+    current = input_file.readline()
+    count = 0
+
+    while current != '':
+        to_return[count] = current.strip()
+        count += 1
+        current = input_file.readline()
+
+    return to_return
+
 
 try:
-    filename = input("Please input the name of the file:")
-    inputFile = open(filename, "r")
-    
-    currentLine = inputFile.readline()
-    count = 0
-    while(currentLine != ""):
-        cur = currentLine.strip()
-        print("Sending: " + cur + " to Wit...")
-        witClient.message(cur)
-        currentLine = inputFile.readline()
-        count = count + 1
+    trainingAmount = int(input("Desired number of training messages:"))
+
+    people = initialize("ListFile")
+    templates = initialize("witData")
+
+    for x in range(trainingAmount):
+        people_index = random.randint(0, len(people)-1)
+        template_index = random.randint(0, len(templates)-1)
+
+        template = templates[template_index].split('[name]')
+
+        if len(template) > 1:
+            message = template[0] + people[people_index] + template[1]
+        else:
+            message = template[0] + people[people_index]
+
+        print("Sending: " + message + " to NLP engine")
+        witClient.message(message)
         time.sleep(3)
 
-    print("Successfully sent " + str(count) + " training messages to Wit.")
-    print("Deleting file...")
-    os.remove(filename)
-    file = open(filename, 'r')
-    inputFile.close()
-    
+    print("\n----------------------------------------------------------")
+    print("I've sent " + str(trainingAmount) + " messages to the NLP engine.")
+    print("----------------------------------------------------------\n")
+
 except Exception as e:
     print(e)
-    print("I'm sorry, the file cannot be found. Exiting...")
+    print("I'm sorry, the required files cannot be found. Exiting...")
     sys.exit()
