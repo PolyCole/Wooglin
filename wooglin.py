f9958ddd6eae42bb39c7cf2859d6f9a426ecd75b
Cole Polyak <colepolyak@gmail.com>
2020-02-27 13:37:26 -0700

    Wooglin responds to all DMs, and direct messages if there's more than one person.

--- wooglin.py
+++ wooglin.py
@@ -47,7 +47,7 @@ def lambda_handler(data, context):
             return "200 OK"
 
     # Ignore other bot events.
-    if "subtype" in slack_event:
+    if "bot_id" in slack_event:
         if slack_event['subtype'] == "bot_message":
             logging.warning("Ignore bot event")
             return "200 OK"
@@ -58,25 +58,37 @@ def lambda_handler(data, context):
         # Getting ID of channel where message originated.
         SLACK_CHANNEL = slack_event["channel"]
 
-        # Checks that the user @ed us.
-        if text.find(os.environ['MY_ID']) != -1:
-
-            # Pulling the nasty @tag out of the message.
-            text = re.sub('<@.........>',"Wooglin,",text).strip()
-            print("Text after @ removal: " + text)
-            try:
-                # Some classics.
-                if text == os.environ['SECRET_PROMPT']:
-                    sendmessage(os.environ['SECRET_RESPONSE'])
-                elif text == "Wooglin, play funkytown":
-                    sendmessage("https://www.youtube.com/watch?v=s36eQwgPNSE")
-                # Not a given response, let's send the message to NLP.
-                else:
-                    process_message(slack_event)
-            except Exception as e:
-                sendmessage("I've encountered an error: " + str(e))
-
+        if 'channel_type' in slack_event and slack_event['channel_type'] == "im":
+            process_message_helper(text, slack_event)
+        else:
+            if mentions_me(text):
+                process_message_helper(strip_text(text), slack_event)
         return "200 OK"
+
+
+def mentions_me(text):
+    return text.find(os.environ['MY_ID']) != -1
+
+
+def strip_text(text):
+    # Pulling the nasty @tag out of the message.
+    text = re.sub('<@.........>', "Wooglin,", text).strip()
+    print("Text after @ removal: " + text)
+    return text
+
+
+def process_message_helper(text, slack_event):
+    try:
+        # Some classics.
+        if text == "Wooglin " + os.environ['SECRET_PROMPT']:
+            sendmessage(os.environ['SECRET_RESPONSE'])
+        elif text == "Wooglin, play funkytown":
+            sendmessage("https://www.youtube.com/watch?v=s36eQwgPNSE")
+        # Not a given response, let's send the message to NLP.
+        else:
+            process_message(slack_event)
+    except Exception as e:
+        sendmessage("I've encountered an error: " + str(e))
 
 
 # Sends a message in slack.
